633d2526094e9a6daf32144e536f7b52f38e1597
kevin.taylor.dr@gmail.com <kevin.taylor.dr@gmail.com@afb9890d-5ef5-9d65-7322-0cae69767e52>
2011-05-25 22:43:01 +0000

    tagging version 1.2.7

--- milmaps/src/main/gwt/com/moesol/gwt/maps/client/TileImageEngineListener.java
+++
@@ -1,8 +0,0 @@
-package com.moesol.gwt.maps.client;
-
-public interface TileImageEngineListener {
-	Object createImage(TileCoords tileCoords);
-	void useImage(TileCoords tileCoords, Object image);
-	void hideImage(Object image);
-	void destroyImage(Object image);
-}
