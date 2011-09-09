02305d6a595867f1d9128bacba5b05c47873f2e9
kevin.taylor.dr@gmail.com <kevin.taylor.dr@gmail.com@afb9890d-5ef5-9d65-7322-0cae69767e52>
2011-09-09 22:02:00 +0000

    Creating a new tag: 1.2.9, also updated rpf tile layer settings

--- milmaps/src/main/gwt/com/moesol/gwt/maps/client/MapLayersOnClient.java
+++ milmaps/src/main/gwt/com/moesol/gwt/maps/client/MapLayersOnClient.java
@@ -22,16 +22,24 @@ public class MapLayersOnClient {
 		layerSet0.setUrlPattern("{server}/tileset/BMNG/{data}/level/{level}/x/{x}/y/{y}");
 	    layerSet0.setStartLevelTileDimensionsInDeg(36.0, 36.0);
 	    layerSet0.withPixelWidth(512).setPixelHeight(512);
+	    layerSet0.setAutoRefreshOnTimer(false);
+	    layerSet0.setZeroTop(false);
+	    layerSet0.setEpsg(4326);
+	    layerSet0.setStartLevel(0);
 	    
-		layerSet1 = new LayerSet();
-		layerSet1.setServer(servletUrl);
-		layerSet1.setData("esat_world");
+		//layerSet1 = new LayerSet();
+		//layerSet1.setServer(servletUrl);
+		//layerSet1.setData("esat_world");
 		//layerSet1.setSkipLevels(4);
-	    layerSet1.setStartLevelTileDimensionsInDeg(2.25, 2.25);
-	    layerSet1.withPixelWidth(512).setPixelHeight(512);
-		layerSet1.setUrlPattern("{server}/tileset/{data}/level/{level}/x/{x}/y/{y}");
+	    //layerSet1.setStartLevelTileDimensionsInDeg(2.25, 2.25);
+	    //layerSet1.withPixelWidth(512).setPixelHeight(512);
+		//layerSet1.setUrlPattern("{server}/tileset/{data}/level/{level}/x/{x}/y/{y}");
+	    //layerSet1.setAutoRefreshOnTimer(false);
+	    //layerSet1.setZeroTop(false);
+	    //layerSet1.setEpsg(4326);
+	    //layerSet1.setStartLevel(0);
 		
-		return new LayerSet[] { layerSet0, layerSet1 };
+		return new LayerSet[] { layerSet0 };//, layerSet1 };
 	}
 
 	private static String getServerUrl() {
