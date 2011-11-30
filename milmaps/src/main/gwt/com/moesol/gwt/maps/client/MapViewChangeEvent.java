49d3e50dead72f8657071cf0d1e2f72370f1c1a5
Matthew.Madson@gmail.com <Matthew.Madson@gmail.com@afb9890d-5ef5-9d65-7322-0cae69767e52>
2011-11-30 21:53:21 +0000

    In response to code review: changed indents to tabs, updated constructors of MapView to ensure EventBus and MapController could be defined as final fields; passed eventBus to mapController instead of implicit tight coupling.

--- milmaps/src/main/gwt/com/moesol/gwt/maps/client/MapViewChangeEvent.java
+++ milmaps/src/main/gwt/com/moesol/gwt/maps/client/MapViewChangeEvent.java
@@ -3,32 +3,31 @@ package com.moesol.gwt.maps.client;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.event.shared.EventBus;
-import com.google.gwt.event.shared.GwtEvent;
 import com.google.gwt.event.shared.HandlerRegistration;
 
 public class MapViewChangeEvent
-        extends ValueChangeEvent<MapView>
+		extends ValueChangeEvent<MapView>
 {
-    public interface Handler
-            extends ValueChangeHandler<MapView>
-    {
-    }
+	public interface Handler
+			extends ValueChangeHandler<MapView>
+	{
+	}
 
-    private static final Type<Handler> TYPE = new Type<Handler>();
+	private static final Type<Handler> TYPE = new Type<Handler>();
 
-    private MapViewChangeEvent(final MapView value)
-    {
-        super(value);
-    }
+	private MapViewChangeEvent(final MapView value)
+	{
+		super(value);
+	}
 
-    public static void fire(final EventBus eventBus, final MapView map)
-    {
-        eventBus.fireEvent(new MapViewChangeEvent(map));
-    }
+	public static void fire(final EventBus eventBus, final MapView map)
+	{
+		eventBus.fireEvent(new MapViewChangeEvent(map));
+	}
 
-    public static HandlerRegistration register(EventBus eventBus,
-            Handler handler)
-    {
-        return eventBus.addHandler(TYPE, handler);
-    }
+	public static HandlerRegistration register(EventBus eventBus,
+			Handler handler)
+	{
+		return eventBus.addHandler(TYPE, handler);
+	}
 }
