8d9dfc5e494f2ff8272627087ac8544bbbc1c770
mr.robert.hastings <mr.robert.hastings@afb9890d-5ef5-9d65-7322-0cae69767e52>
2012-01-06 05:58:02 +0000

    Tuned up the flyto. Use spline interpolator for pan to ease into the pan and then ease out of it. Zoom out more quickly. Then Zoom in at 40% until complete.

--- trunk/milmaps/src/test/java/com/moesol/gwt/maps/client/FlyToEngineTest.java
+++ trunk/milmaps/src/test/java/com/moesol/gwt/maps/client/FlyToEngineTest.java
@@ -32,33 +32,36 @@ public class FlyToEngineTest {
 	}
 	
 	@Test
-	public void when_25_percent_then_zoom_out_half() {
+	public void when_zoom_out_half_then_correct_scale() {
 		engine.initEngine(20, 30, 1/50000.0);
-		engine.onUpdate(0.25);
+		engine.onUpdate(FlyToEngine.ZOOM_OUT_UNTIL / 2);
 		
 		MapScale result = new JmvMapScale(proj.getScale());
-		MapScale expect = JmvMapScale.parse("1:34.1M");
+		MapScale expect = JmvMapScale.parse("1:107.4M");
 		
 		assertEquals(expect.toString(), result.toString());
 	}
 	
 	@Test
-	public void when_50_percent_then_zoom_out_full() {
+	public void when_zoom_out_full_then_target_zoom_out_scale() {
 		engine.initEngine(20, 30, 1/50000.0);
-		engine.onUpdate(0.50);
+		engine.onUpdate(FlyToEngine.ZOOM_OUT_UNTIL);
 		
-		assertEquals(FlyToEngine.ZOOM_OUT_TARGET_SCALE, proj.getScale(), 0.0);
+		MapScale expect = JmvMapScale.parse("1:100M");
+		MapScale result = new JmvMapScale(proj.getScale());
+		
+		assertEquals(expect.toString(), result.toString());
 	}
 	
 	@Test
 	public void when_1_percent_then_pan_started() {
 		engine.initEngine(20, 30, 1/50000.0);
-		engine.onUpdate(0.01);
+		engine.onUpdate(0.01 * FlyToEngine.PAN_UNTIL);
 		
 		ArgumentCaptor<GeodeticCoords> cap = ArgumentCaptor.forClass(GeodeticCoords.class);
 		verify(mapView).setCenter(cap.capture());
-		assertEquals(0.2, cap.getValue().getPhi(AngleUnit.DEGREES), 0.0);
-		assertEquals(0.3, cap.getValue().getLambda(AngleUnit.DEGREES), 0.0);
+		assertTrue(cap.getValue().getPhi(AngleUnit.DEGREES) > 0.0);
+		assertTrue(cap.getValue().getLambda(AngleUnit.DEGREES) > 0.0);
 	}
 
 	@Test
@@ -68,17 +71,17 @@ public class FlyToEngineTest {
 		
 		ArgumentCaptor<GeodeticCoords> cap = ArgumentCaptor.forClass(GeodeticCoords.class);
 		verify(mapView).setCenter(cap.capture());
-		assertEquals(20.0, cap.getValue().getPhi(AngleUnit.DEGREES), 0.0);
+		assertEquals(20.0, cap.getValue().getPhi(AngleUnit.DEGREES), 0.00001);
 		assertEquals(30.0, cap.getValue().getLambda(AngleUnit.DEGREES), 0.00001);
 	}
 	
 	@Test
 	public void when_51_percent_then_zoom_in_started() {
 		engine.initEngine(20, 30, 1/50000.0);
-		engine.onUpdate(0.51);
+		engine.onUpdate(FlyToEngine.ZOOM_IN_AT + 0.1);
 		
 		MapScale result = new JmvMapScale(proj.getScale());
-		MapScale expect = JmvMapScale.parse("1:2.2M");
+		MapScale expect = JmvMapScale.parse("1:1.4M");
 		assertEquals(expect.toString(), result.toString());
 	}
 	
@@ -89,56 +92,56 @@ public class FlyToEngineTest {
 		
 		ArgumentCaptor<GeodeticCoords> cap = ArgumentCaptor.forClass(GeodeticCoords.class);
 		verify(mapView).setCenter(cap.capture());
-		assertEquals(20.0, cap.getValue().getPhi(AngleUnit.DEGREES), 0.0);
+		assertEquals(20.0, cap.getValue().getPhi(AngleUnit.DEGREES), 0.00001);
 		assertEquals(30.0, cap.getValue().getLambda(AngleUnit.DEGREES), 0.00001);
-		assertEquals(1/50000.0, proj.getScale(), 0.0);
+		assertEquals(1/50000.0, proj.getScale(), 0.1);
 	}
 	
 	@Test
 	public void when_crossing_180_right_to_left_at_start_then_moving_shortest_direction() {
 		proj.setViewGeoCenter(new GeodeticCoords(-179.0, 0.0, AngleUnit.DEGREES));
 		engine.initEngine(0.0, 179.0, 1/50000.0);
-		engine.onUpdate(0.35);
+		engine.onUpdate(0.35 * FlyToEngine.PAN_UNTIL);
 		
 		ArgumentCaptor<GeodeticCoords> cap = ArgumentCaptor.forClass(GeodeticCoords.class);
 		verify(mapView).setCenter(cap.capture());
 		assertEquals(0.0, cap.getValue().getPhi(AngleUnit.DEGREES), 0.0);
-		assertEquals(-179.7, cap.getValue().getLambda(AngleUnit.DEGREES), 0.00001);
+		assertEquals(-179.48054, cap.getValue().getLambda(AngleUnit.DEGREES), 0.00001);
 	}
 
 	@Test
 	public void when_crossing_180_right_to_left_at_finish_then_pan_finsish() {
 		proj.setViewGeoCenter(new GeodeticCoords(-179.0, 0.0, AngleUnit.DEGREES));
 		engine.initEngine(0.0, 179.0, 1/50000.0);
-		engine.onUpdate(1.0);
+		engine.onUpdate(FlyToEngine.PAN_UNTIL);
 		
 		ArgumentCaptor<GeodeticCoords> cap = ArgumentCaptor.forClass(GeodeticCoords.class);
 		verify(mapView).setCenter(cap.capture());
 		assertEquals(0.0, cap.getValue().getPhi(AngleUnit.DEGREES), 0.0);
-		assertEquals(179.0, cap.getValue().getLambda(AngleUnit.DEGREES), 0.00001);
+		assertEquals(179.0, cap.getValue().getLambda(AngleUnit.DEGREES), 0.001);
 	}
 
 	@Test
 	public void when_crossing_180_left_to_right_at_start_then_moving_shortest_direction() {
 		proj.setViewGeoCenter(new GeodeticCoords(179.0, 0.0, AngleUnit.DEGREES));
 		engine.initEngine(0.0, -179.0, 1/50000.0);
-		engine.onUpdate(0.35);
+		engine.onUpdate(0.35 * FlyToEngine.PAN_UNTIL);
 		
 		ArgumentCaptor<GeodeticCoords> cap = ArgumentCaptor.forClass(GeodeticCoords.class);
 		verify(mapView).setCenter(cap.capture());
 		assertEquals(0.0, cap.getValue().getPhi(AngleUnit.DEGREES), 0.0);
-		assertEquals(179.7, cap.getValue().getLambda(AngleUnit.DEGREES), 0.00001);
+		assertEquals(179.48054, cap.getValue().getLambda(AngleUnit.DEGREES), 0.00001);
 	}
 
 	@Test
 	public void when_crossing_180_left_to_right_at_finish_then_pan_finsish() {
 		proj.setViewGeoCenter(new GeodeticCoords(179.0, 0.0, AngleUnit.DEGREES));
 		engine.initEngine(0.0, -179.0, 1/50000.0);
-		engine.onUpdate(1.0);
+		engine.onUpdate(FlyToEngine.PAN_UNTIL);
 		
 		ArgumentCaptor<GeodeticCoords> cap = ArgumentCaptor.forClass(GeodeticCoords.class);
 		verify(mapView).setCenter(cap.capture());
 		assertEquals(0.0, cap.getValue().getPhi(AngleUnit.DEGREES), 0.0);
-		assertEquals(-179.0, cap.getValue().getLambda(AngleUnit.DEGREES), 0.00001);
+		assertEquals(-179.0, cap.getValue().getLambda(AngleUnit.DEGREES), 0.001);
 	}
 }
