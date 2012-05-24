8e83bffb34f918d3d2c83f7794210b416d277274
axlepastel@gmail.com <axlepastel@gmail.com@afb9890d-5ef5-9d65-7322-0cae69767e52>
2012-05-24 22:18:45 +0000

    Updating license headers to reference GPLv3

--- branches/milmaps/src/test/java/com/moesol/gwt/maps/client/timing/interpolators/SplineInterpolatorTest.java
+++ branches/milmaps/src/test/java/com/moesol/gwt/maps/client/timing/interpolators/SplineInterpolatorTest.java
@@ -1,11 +1,9 @@
 /**
- * (c) Copyright, Moebius Solutions, Inc., 2006
+ * (c) Copyright, Moebius Solutions, Inc., 2012
  *
  *                        All Rights Reserved
  *
- * This material may be reproduced by or for the U. S. Government
- * pursuant to the copyright license under the clause at
- * DFARS 252.227-7014 (OCT 2001).
+ * LICENSE: GPLv3
  */
 package com.moesol.gwt.maps.client.timing.interpolators;
 
