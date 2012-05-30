b91279c4be293b21ed47292213d892ef37af7eec
axlepastel@gmail.com <axlepastel@gmail.com@afb9890d-5ef5-9d65-7322-0cae69767e52>
2012-05-30 22:28:27 +0000

    Show bubble when touching a track

--- branches/branch-2.0/milmapsMobile/src/main/java/com/milmaps/client/MapTouchController.java
+++ branches/branch-2.0/milmapsMobile/src/main/java/com/milmaps/client/MapTouchController.java
@@ -1,5 +1,6 @@
 package com.milmaps.client;
 
+import com.google.gwt.core.client.JsArray;
 import com.google.gwt.dom.client.Touch;
 import com.google.gwt.event.dom.client.GestureChangeEvent;
 import com.google.gwt.event.dom.client.GestureChangeHandler;
@@ -15,135 +16,208 @@ import com.google.gwt.event.dom.client.TouchMoveEvent;
 import com.google.gwt.event.dom.client.TouchMoveHandler;
 import com.google.gwt.event.dom.client.TouchStartEvent;
 import com.google.gwt.event.dom.client.TouchStartHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.FocusPanel;
 import com.google.gwt.user.client.ui.HasText;
 import com.moesol.gwt.maps.client.DragTracker;
+import com.moesol.gwt.maps.client.GeodeticCoords;
+import com.moesol.gwt.maps.client.LayerSet;
+import com.moesol.gwt.maps.client.LayerSetJson;
 import com.moesol.gwt.maps.client.MapView;
+import com.moesol.gwt.maps.client.ViewCoords;
+import com.moesol.gwt.maps.client.ViewWorker;
 import com.moesol.gwt.maps.client.WorldCoords;
+import com.moesol.gwt.maps.client.controls.BubbleControl;
+import com.moesol.gwt.maps.client.tms.FeatureReader;
+import com.moesol.gwt.maps.client.units.AngleUnit;
+import com.moesol.gwt.maps.shared.Feature;
+import java.util.List;
 
-public class MapTouchController implements 
-	TouchStartHandler, TouchMoveHandler, TouchEndHandler, TouchCancelHandler, 
-	GestureStartHandler, GestureChangeHandler, GestureEndHandler
-{
-	private final MapView m_map;
-	private DragTracker m_dragTracker;
-	
-	private HasText m_msg = new HasText() {
-		@Override
-		public void setText(String text) {
-		}
-		@Override
-		public String getText() {
-			return null;
-		}
-	};
+public class MapTouchController implements
+        TouchStartHandler, TouchMoveHandler, TouchEndHandler, TouchCancelHandler,
+        GestureStartHandler, GestureChangeHandler, GestureEndHandler {
 
-	public MapTouchController(MapView map) {
-		m_map = map;
-	}
+    private final MapView m_mapView;
+    private DragTracker m_dragTracker;
+    private FeatureReader m_reader;
+    private BubbleControl m_bubbleControl;
+    private HasText m_msg = new HasText() {
 
-	public MapTouchController withMsg(HasText msg) {
-		m_msg = msg;
-		return this;
-	}
+        @Override
+        public void setText(String text) {
+        }
 
-	public void bindHandlers(FocusPanel touchPanel) {
-		touchPanel.addTouchStartHandler(this);
-		touchPanel.addTouchMoveHandler(this);
-		touchPanel.addTouchEndHandler(this);
-		touchPanel.addTouchCancelHandler(this);
-		touchPanel.addGestureStartHandler(this);
-		touchPanel.addGestureChangeHandler(this);
-		touchPanel.addGestureEndHandler(this);
-	}
+        @Override
+        public String getText() {
+            return null;
+        }
+    };
 
-	private void maybeDragMap(int x, int y) {
-		if (m_dragTracker == null) {
-			return; // Not dragging
-		}
-		WorldCoords newWorldCenter = m_dragTracker.update(x, y);
-		if (m_dragTracker.isSameAsLast()) {
-			return;
-		}
-		
-		m_map.cancelAnimations();
-		m_map.setWorldCenter(newWorldCenter);
-		m_map.partialUpdateView();
-	}
-	
-	@Override
-	public void onTouchStart(TouchStartEvent event) {
-		m_msg.setText("ts: " + event.getTargetTouches().length());
-		event.preventDefault();
-		
-		switch (event.getTargetTouches().length()) {
-		case 1: {
-			Touch touch = event.getTargetTouches().get(0);
-			m_dragTracker = new DragTracker(
-					touch.getPageX(), touch.getPageY(), m_map.getWorldCenter());
-			break;
-		}
-		default:
-			m_dragTracker = null;
-		}
-	}
-	
-	@Override
-	public void onTouchMove(TouchMoveEvent event) {
-		m_msg.setText("tm: " + event.getTargetTouches().length());
-		event.preventDefault();
-		
-		switch (event.getTargetTouches().length()) {
-		case 1: {
-			Touch touch = event.getTargetTouches().get(0);
-			maybeDragMap(touch.getPageX(), touch.getPageY());
-			break;
-		}
-		default:
-			m_dragTracker = null;
-		}
-	}
-	
-	@Override
-	public void onTouchEnd(TouchEndEvent event) {
-		m_msg.setText("te: " + event.getTargetTouches().length());
-		event.preventDefault();
-		if (m_dragTracker != null) {
-			m_dragTracker = null;
-			m_map.dumbUpdateView();
-		}
-	}
-	
-	@Override
-	public void onTouchCancel(TouchCancelEvent event) {
-		m_msg.setText("tc: " + event.getTargetTouches().length());
-		event.preventDefault();
-		if (m_dragTracker != null) {
-			m_dragTracker = null;
-			m_map.dumbUpdateView();
-		}
-	}
+    /** Hook out to layerSet json data */
+    private final native JsArray<LayerSetJson> getLayerSets() /*-{
+     return $wnd.layerSets;
+     }-*/;
 
-	@Override
-	public void onGestureStart(GestureStartEvent event) {
-		m_msg.setText("gs: " + event.toDebugString());
-		event.preventDefault();
-		//
-		// m_map.animateZoom(event.getScale());
-	}
+    public MapTouchController(MapView map) {
+        m_mapView = map;
+        String server = "";
+        JsArray<LayerSetJson> layerSets = getLayerSets();
+        if (layerSets != null) {
+            for (int i = 0, n = layerSets.length(); i < n; ++i) {
+                LayerSetJson l = layerSets.get(i);
+                LayerSet ls = l.toLayerSet();
+                if (ls.getId().equals("pli")) {
+                    server = ls.getServer();
+                    break;
+                }
+            }
+        }
+        String tileUrlFormatPrefix = "{server}/rs/tiles/";
+        FeatureReader.Builder builder = new FeatureReader.Builder(server, tileUrlFormatPrefix
+                + "{level}/{lat}/{lng}/features");
 
-	@Override
-	public void onGestureChange(GestureChangeEvent event) {
-		m_msg.setText("gc: " + event.toDebugString());
-		event.preventDefault();
-		
-		// m_map.animateZoom(event.getScale());
-	}
+        m_reader = builder.build();
+        m_bubbleControl = new BubbleControl(m_mapView);
+    }
 
-	@Override
-	public void onGestureEnd(GestureEndEvent event) {
-		m_msg.setText("ge: " + event.toDebugString());
-		event.preventDefault();
-	}
+    public MapTouchController withMsg(HasText msg) {
+        m_msg = msg;
+        return this;
+    }
 
+    public void bindHandlers(FocusPanel touchPanel) {
+        touchPanel.addTouchStartHandler(this);
+        touchPanel.addTouchMoveHandler(this);
+        touchPanel.addTouchEndHandler(this);
+        touchPanel.addTouchCancelHandler(this);
+        touchPanel.addGestureStartHandler(this);
+        touchPanel.addGestureChangeHandler(this);
+        touchPanel.addGestureEndHandler(this);
+    }
+
+    private void maybeDragMap(int x, int y) {
+        if (m_dragTracker == null) {
+            return; // Not dragging
+        }
+        WorldCoords newWorldCenter = m_dragTracker.update(x, y);
+        if (m_dragTracker.isSameAsLast()) {
+            return;
+        }
+
+        m_mapView.cancelAnimations();
+        m_mapView.setWorldCenter(newWorldCenter);
+        m_mapView.partialUpdateView();
+    }
+
+    @Override
+    public void onTouchStart(TouchStartEvent event) {
+        m_msg.setText("ts: " + event.getTargetTouches().length());
+        event.preventDefault();
+
+        switch (event.getTargetTouches().length()) {
+            case 1: {
+                final Touch touch = event.getTargetTouches().get(0);
+                m_dragTracker = new DragTracker(
+                        touch.getPageX(), touch.getPageY(), m_mapView.getWorldCenter());
+                maybeShowBubble(touch);
+                break;
+            }
+            default:
+                m_dragTracker = null;
+        }
+    }
+
+    private void maybeShowBubble(final Touch touch) {
+        ViewCoords m_vc = new ViewCoords(touch.getPageX(), touch.getPageY());
+        ViewWorker viewWorker = m_mapView.getViewport().getVpWorker();
+        WorldCoords worldCoords = viewWorker.viewToWorld(m_vc);
+        GeodeticCoords gc = m_mapView.getProjection().worldToGeodetic(worldCoords);
+
+        int level = m_mapView.getDivManager().getCurrentLevel();
+        double lat = gc.getPhi(AngleUnit.DEGREES);
+        double lng = gc.getLambda(AngleUnit.DEGREES);
+
+        m_reader.getFeatures(level, lat, lng,
+                new AsyncCallback<List<Feature>>() {
+
+                    @Override
+                    public void onSuccess(List<Feature> result) {
+                        if (result.size() > 0) {
+                            m_bubbleControl.hide();
+                            String titles = "";
+                            for (Feature feature : result) {
+                                titles += feature.getTitle() + "<br/>";
+                            }
+                            m_bubbleControl.getHtml().setHTML(titles);
+                            m_bubbleControl.show(touch.getClientX(), touch.getClientY() - 20);
+                        } else {
+                            m_bubbleControl.hide();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Throwable caught) {
+                        Window.alert("failure");
+                    }
+                });
+    }
+
+    @Override
+    public void onTouchMove(TouchMoveEvent event) {
+        m_msg.setText("tm: " + event.getTargetTouches().length());
+        event.preventDefault();
+
+        switch (event.getTargetTouches().length()) {
+            case 1: {
+                Touch touch = event.getTargetTouches().get(0);
+                maybeDragMap(touch.getPageX(), touch.getPageY());
+                break;
+            }
+            default:
+                m_dragTracker = null;
+        }
+    }
+
+    @Override
+    public void onTouchEnd(TouchEndEvent event) {
+        m_msg.setText("te: " + event.getTargetTouches().length());
+        event.preventDefault();
+        if (m_dragTracker != null) {
+            m_dragTracker = null;
+            m_mapView.dumbUpdateView();
+        }
+    }
+
+    @Override
+    public void onTouchCancel(TouchCancelEvent event) {
+        m_msg.setText("tc: " + event.getTargetTouches().length());
+        event.preventDefault();
+        if (m_dragTracker != null) {
+            m_dragTracker = null;
+            m_mapView.dumbUpdateView();
+        }
+    }
+
+    @Override
+    public void onGestureStart(GestureStartEvent event) {
+        m_msg.setText("gs: " + event.toDebugString());
+        event.preventDefault();
+        //
+        // m_map.animateZoom(event.getScale());
+    }
+
+    @Override
+    public void onGestureChange(GestureChangeEvent event) {
+        m_msg.setText("gc: " + event.toDebugString());
+        event.preventDefault();
+
+        // m_map.animateZoom(event.getScale());
+    }
+
+    @Override
+    public void onGestureEnd(GestureEndEvent event) {
+        m_msg.setText("ge: " + event.toDebugString());
+        event.preventDefault();
+    }
 }
--- branches/branch-2.0/milmapsMobile/src/main/java/com/milmaps/client/MobileEntryPoint.java
+++ branches/branch-2.0/milmapsMobile/src/main/java/com/milmaps/client/MobileEntryPoint.java
@@ -27,20 +27,20 @@ import com.moesol.gwt.maps.client.MapView;
 
 public class MobileEntryPoint implements EntryPoint {
 
-	private MapView m_map;
-	private Label m_msg;
+    private MapView m_mapView;
+    private Label m_msg;
     private FocusPanel m_touchPanel;
     private AbsolutePanel m_controlsAndMap;
 
-	public MobileEntryPoint() {
-		super();
-	}
+    public MobileEntryPoint() {
+        super();
+    }
 
-	@Override
-	public void onModuleLoad() {
-		final RootPanel mapPanel = RootPanel.get("mapPanel");
-		if (mapPanel != null) {
-			doMapPanel(mapPanel);
+    @Override
+    public void onModuleLoad() {
+        final RootPanel mapPanel = RootPanel.get("mapPanel");
+        if (mapPanel != null) {
+            doMapPanel(mapPanel);
             Window.addResizeHandler(new ResizeHandler() {
 
                 @Override
@@ -48,98 +48,99 @@ public class MobileEntryPoint implements EntryPoint {
                     setView();
                 }
             });
-		}
-	}
+        }
+    }
 
-	/** Hook out to layerSet json data */
-	private final native JsArray<LayerSetJson> getLayerSets() /*-{
-    	return $wnd.layerSets;
-	}-*/;
+    /** Hook out to layerSet json data */
+    private final native JsArray<LayerSetJson> getLayerSets() /*-{
+     return $wnd.layerSets;
+     }-*/;
 
-	private void doMapPanel(RootPanel mapPanel) {
-		DOM.setInnerHTML(mapPanel.getElement(), "");
-		m_touchPanel = makeTouchPanel();
-		m_controlsAndMap = new AbsolutePanel();
-		
-		m_msg = new Label("msg...");
-		m_map = new MapView();
-		loadLayerConfigsFromClient();
-		setView();
-		bindListeners(m_touchPanel, m_map);
-		
-		Button in = new Button(" + ", new ClickHandler() {
-			@Override
-			public void onClick(ClickEvent event) {
-				goIn();
-			}
-		});
-		in.setStylePrimaryName("toolButton");
-		Button out = new Button(" - ", new ClickHandler() {
-			@Override
-			public void onClick(ClickEvent event) {
-				goOut();
-			}
-		});
-		out.setStylePrimaryName("toolButton");
+    private void doMapPanel(RootPanel mapPanel) {
+        DOM.setInnerHTML(mapPanel.getElement(), "");
+        m_touchPanel = makeTouchPanel();
+        m_controlsAndMap = new AbsolutePanel();
 
-		VerticalPanel bar = new VerticalPanel();
-		bar.add(in);
-		bar.add(out);
-		bar.add(m_msg);
-		bar.getElement().getStyle().setZIndex(5000);
-		bar.getElement().setId("buttonBar");
-		
-		m_touchPanel.setWidget(m_map);
-		m_controlsAndMap.add(m_touchPanel);
-		m_controlsAndMap.add(bar);
-		m_controlsAndMap.setWidgetPosition(bar, 0, 20);
-		m_controlsAndMap.setWidgetPosition(m_touchPanel, 0, 0);
-		mapPanel.add(m_controlsAndMap);
-	}
+        m_msg = new Label("msg...");
+        m_mapView = new MapView();
+        loadLayerConfigsFromClient();
+        setView();
+        bindListeners(m_touchPanel, m_mapView);
+
+        Button in = new Button(" + ", new ClickHandler() {
+
+            @Override
+            public void onClick(ClickEvent event) {
+                goIn();
+            }
+        });
+        in.setStylePrimaryName("toolButton");
+        Button out = new Button(" - ", new ClickHandler() {
+
+            @Override
+            public void onClick(ClickEvent event) {
+                goOut();
+            }
+        });
+        out.setStylePrimaryName("toolButton");
+
+        VerticalPanel bar = new VerticalPanel();
+        bar.add(in);
+        bar.add(out);
+        bar.add(m_msg);
+        bar.getElement().getStyle().setZIndex(5000);
+        bar.getElement().setId("buttonBar");
+
+        m_touchPanel.setWidget(m_mapView);
+        m_controlsAndMap.add(m_touchPanel);
+        m_controlsAndMap.add(bar);
+        m_controlsAndMap.setWidgetPosition(bar, 0, 20);
+        m_controlsAndMap.setWidgetPosition(m_touchPanel, 0, 0);
+        mapPanel.add(m_controlsAndMap);
+    }
 
     private void setView() {
         int w = Window.getClientWidth();
         int h = Window.getClientHeight();
-        
+
         m_controlsAndMap.setPixelSize(w, h);
         m_touchPanel.setPixelSize(w, h);
-        m_map.resizeMap(w, h);
-        m_map.updateView();
+        m_mapView.resizeMap(w, h);
+        m_mapView.updateView();
     }
 
-	private void bindListeners(FocusPanel touchPanel, MapView map) {
-		MapTouchController controller = new MapTouchController(map);
-		controller.bindHandlers(touchPanel);
-		controller.withMsg(m_msg);
-	}
-    
-	private FocusPanel makeTouchPanel() {
-		FocusPanel touchPanel = new FocusPanel();
-		touchPanel.setStyleName("touchPanel");
-		
-		return touchPanel;
-	}
+    private void bindListeners(FocusPanel touchPanel, MapView map) {
+        MapTouchController controller = new MapTouchController(map);
+        controller.bindHandlers(touchPanel);
+        controller.withMsg(m_msg);
+    }
 
-	private void loadLayerConfigsFromClient() {
-		JsArray<LayerSetJson> layerSets = getLayerSets();
-		if (layerSets == null) {
-			return;
-		}
-		for (int i = 0, n = layerSets.length(); i < n; ++i) {
-			LayerSetJson l = layerSets.get(i);
-			LayerSet ls = l.toLayerSet();
-			m_map.addLayer(ls);
-		}
-	}
+    private FocusPanel makeTouchPanel() {
+        FocusPanel touchPanel = new FocusPanel();
+        touchPanel.setStyleName("touchPanel");
 
-	protected void goIn() {
-		m_map.animateZoom(2);
-		//m_map.updateView();
-	}
-	
-	protected void goOut() {
-		m_map.animateZoom(1.0/2.0);
-		//m_map.updateView();
-	}
+        return touchPanel;
+    }
 
+    private void loadLayerConfigsFromClient() {
+        JsArray<LayerSetJson> layerSets = getLayerSets();
+        if (layerSets == null) {
+            return;
+        }
+        for (int i = 0, n = layerSets.length(); i < n; ++i) {
+            LayerSetJson l = layerSets.get(i);
+            LayerSet ls = l.toLayerSet();
+            m_mapView.addLayer(ls);
+        }
+    }
+
+    protected void goIn() {
+        m_mapView.animateZoom(2);
+        //m_map.updateView();
+    }
+
+    protected void goOut() {
+        m_mapView.animateZoom(1.0 / 2.0);
+        //m_map.updateView();
+    }
 }
