7da3c09269019e919e6d367933ca392a94192a2a
axlepastel@gmail.com <axlepastel@gmail.com@afb9890d-5ef5-9d65-7322-0cae69767e52>
2012-06-01 21:24:34 +0000

    Double-tap to zoom Added license headers

--- branches/branch-2.0/milmapsMobile/src/main/java/com/milmaps/client/MobileEntryPoint.java
+++ branches/branch-2.0/milmapsMobile/src/main/java/com/milmaps/client/MobileEntryPoint.java
@@ -1,8 +1,8 @@
-/*
- * $Id$
- *
+/**
  * (c) Copyright, Moebius Solutions, Inc., 2012
- * 
+ *
+ *                        All Rights Reserved
+ *
  * LICENSE: GPLv3
  */
 package com.milmaps.client;
