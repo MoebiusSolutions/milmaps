13f49a9ec9446d6378fc625650941a0e98dd93c8
kevin.taylor.dr@gmail.com <kevin.taylor.dr@gmail.com@afb9890d-5ef5-9d65-7322-0cae69767e52>
2012-06-22 15:55:23 +0000

    we will no longer have a trunk

--- trunk/milmaps/src/main/gwt/com/moesol/gwt/maps/client/controls/HoverHandler.java
+++
@@ -1,7 +0,0 @@
-package com.moesol.gwt.maps.client.controls;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface HoverHandler extends EventHandler {
-	void onHover(HoverEvent hoverEvent);
-}
