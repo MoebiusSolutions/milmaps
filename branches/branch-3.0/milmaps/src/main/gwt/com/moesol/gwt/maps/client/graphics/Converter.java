d52b9779d46cf9416849a49986b4c6dc2d82f3de
kevin.taylor.dr@gmail.com <kevin.taylor.dr@gmail.com@afb9890d-5ef5-9d65-7322-0cae69767e52>
2012-08-03 19:13:05 +0000

    I refactored Converter so I could unit test it and wrote a basic unit test

--- branches/branch-3.0/milmaps/src/main/gwt/com/moesol/gwt/maps/client/graphics/Converter.java
+++ branches/branch-3.0/milmaps/src/main/gwt/com/moesol/gwt/maps/client/graphics/Converter.java
@@ -9,88 +9,87 @@ package com.moesol.gwt.maps.client.graphics;
 
 import com.moesol.gwt.maps.client.GeodeticCoords;
 import com.moesol.gwt.maps.client.IProjection;
-import com.moesol.gwt.maps.client.MapView;
 import com.moesol.gwt.maps.client.ViewCoords;
+import com.moesol.gwt.maps.client.ViewPort;
 import com.moesol.gwt.maps.client.ViewWorker;
 import com.moesol.gwt.maps.client.WorldCoords;
 
+
 public class Converter implements  ICoordConverter{
 	
-	protected MapView m_map;
+	protected ViewPort m_viewPort;
 	protected ISplit m_splitter = null;
 	
 	public Converter(){
 	}
 	
+	public Converter(ViewPort vp){
+		m_viewPort = vp;
+		m_splitter = new Splitter(vp);
+	}
+	// CheckForExceptions
+	
+	protected void checkForExceptios(){
+		if (m_viewPort == null) {
+			throw new IllegalStateException("Converter: m_viewPort = null");
+		}
+	}
+	
 	@Override
-	public void setMap(MapView map) {
-		m_map = map;
-		m_splitter = new Splitter(map.getViewport());
+	public void setViewPort(ViewPort vp) {
+		m_viewPort = vp;
+		m_splitter = new Splitter(vp);
 	}
 	
 	// Coordinate conversion
 	@Override
 	public ViewCoords geodeticToView(GeodeticCoords gc) {
-		if (m_map == null) {
-			throw new IllegalStateException("ConverterBase: m_map = null");
-		}
-		ViewWorker vw = m_map.getViewport().getVpWorker();
+		checkForExceptios();
+		ViewWorker vw = m_viewPort.getVpWorker();
 		return vw.geodeticToView(gc);
 	}
 
 	@Override
 	public GeodeticCoords viewToGeodetic(ViewCoords vc) {
-		if (m_map == null) {
-			throw new IllegalStateException("ConverterBase: m_map = null");
-		}
-		ViewWorker vw = m_map.getViewport().getVpWorker();
+		checkForExceptios();
+		ViewWorker vw = m_viewPort.getVpWorker();
 		WorldCoords wc = vw.viewToWorld(vc);
-		IProjection proj = m_map.getProjection();
+		IProjection proj = m_viewPort.getProjection();
 		return proj.worldToGeodetic(wc);
 	}
 
 	@Override
 	public WorldCoords geodeticToWorld(GeodeticCoords gc) {
-		if (m_map == null) {
-			throw new IllegalStateException("ConverterBase: m_map = null");
-		}
-		IProjection proj = m_map.getProjection();
+		checkForExceptios();
+		IProjection proj = m_viewPort.getProjection();
 		return proj.geodeticToWorld(gc);
 	}
 
 	@Override
 	public GeodeticCoords worldToGeodetic(WorldCoords wc) {
-		if (m_map == null) {
-			throw new IllegalStateException("ConverterBase: m_map = null");
-		}
-		IProjection proj = m_map.getProjection();
+		checkForExceptios();
+		IProjection proj = m_viewPort.getProjection();
 		return proj.worldToGeodetic(wc);
 	}
 	
 	@Override
 	public int mapWidth(){
-		if (m_map == null) {
-			throw new IllegalStateException("ConverterBase: m_map = null");
-		}
-		IProjection proj = m_map.getProjection();
+		checkForExceptios();
+		IProjection proj = m_viewPort.getProjection();
 		return proj.iMapWidth();
 	}
 	
 	@Override
 	public ViewCoords worldToView(WorldCoords wc){
-		if (m_map == null) {
-			throw new IllegalStateException("ConverterBase: m_map = null");
-		}
-		ViewWorker vw = m_map.getViewport().getVpWorker();
+		checkForExceptios();
+		ViewWorker vw = m_viewPort.getVpWorker();
 		return vw.wcToVC(wc);
 	}
 	
 	@Override
 	public  WorldCoords viewToWorld(ViewCoords vc){
-		if (m_map == null) {
-			throw new IllegalStateException("ConverterBase: m_map = null");
-		}
-		ViewWorker vw = m_map.getViewport().getVpWorker();
+		checkForExceptios();
+		ViewWorker vw = m_viewPort.getVpWorker();
 		return vw.viewToWorld(vc);
 	}
 
