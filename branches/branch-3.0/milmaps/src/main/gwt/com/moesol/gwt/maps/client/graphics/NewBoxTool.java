d9711cafb87eeb2af5afa4e2d130fdd3fd29bafb
taylork <kevin@moesol.com>
2015-04-15 17:08:35 -0700

    added apache license header to java files

--- branches/branch-3.0/milmaps/src/main/gwt/com/moesol/gwt/maps/client/graphics/NewBoxTool.java
+++ branches/branch-3.0/milmaps/src/main/gwt/com/moesol/gwt/maps/client/graphics/NewBoxTool.java
@@ -7,6 +7,26 @@
  */
 package com.moesol.gwt.maps.client.graphics;
 
+/*
+ * #%L
+ * milmaps
+ * %%
+ * Copyright (C) 2015 Moebius Solutions, Inc.
+ * %%
+ * Copyright 2015 Moebius Solutions Inc
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * #L%
+ */
+
+
 import com.moesol.gwt.maps.client.GeodeticCoords;
 import com.moesol.gwt.maps.client.ViewCoords;
 import com.moesol.gwt.maps.client.algorithms.RangeBearingS;
